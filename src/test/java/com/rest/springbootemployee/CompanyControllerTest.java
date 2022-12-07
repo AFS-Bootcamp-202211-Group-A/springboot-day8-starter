7f3a57efcc83f44abe5eca723f89be5f240d6ab4
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-07 21:47:36 +0800

    [mockito] test: should_get_all_companies_when_perform_get_given_companies

---
+++ src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
@@ -0,0 +1,44 @@
+package com.rest.springbootemployee;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyControllerTest {
+    @Autowired
+    MockMvc client;
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @BeforeEach
+    void cleanRepository() {
+        companyRepository.clearAll();
+    }
+
+    @Test
+    void should_get_all_companies_when_perform_get_given_companies() throws Exception {
+        // given
+        companyRepository.create(new Company(1, "c1", null));
+        companyRepository.create(new Company(2, "c2", null));
+        // when & then
+        client.perform(MockMvcRequestBuilders.get("/companies"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("c1"));
+    }
+
+
+}
