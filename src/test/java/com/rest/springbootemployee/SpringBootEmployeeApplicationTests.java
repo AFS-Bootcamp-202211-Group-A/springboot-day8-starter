739cb9bf6474ae411218fa6ac4f696aca04bfc2e
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-07 12:10:47 +0800

    test: add test should_get_all_employees_when_perform_get_given_employees

--- src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -1,13 +1,49 @@
 package com.rest.springbootemployee;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootEmployeeApplicationTests {
 
+	@Autowired
+	MockMvc client;
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@BeforeEach
+	void cleanRepository() {
+			employeeRepository.clearAll();
+	}
+
 	@Test
-	void contextLoads() {
+	void should_get_all_employees_when_perform_get_given_employees() throws Exception {
+	    //given
+	    employeeRepository.create(new Employee(1, "Susan", 20, "Female", 10000));
+		employeeRepository.create(new Employee(2, "Bob", 21, "Male", 5000));
+	    //when
+	    client.perform(MockMvcRequestBuilders.get("/employees"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].age", containsInAnyOrder(20,21)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].gender", containsInAnyOrder("Female", "Male")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].salary", containsInAnyOrder(10000, 5000)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].name", containsInAnyOrder("Susan", "Bob")));
+
+	    //then
 	}
 
 }
