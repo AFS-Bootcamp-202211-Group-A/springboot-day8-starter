06aef09bdf29079239e64b31dc180771fea643b8
Hezz <heinrich@connect.hku.hk>
2022-12-07 14:01:33 +0800

    test: should_get_2_employees_when_perform_get_by_page_given_employees

--- src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.rest.springbootemployee;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -76,6 +77,27 @@ class SpringBootEmployeeApplicationTests {
 				.andExpect(MockMvcResultMatchers.jsonPath("$[*].salary", containsInAnyOrder(10000, 10000)));
 
 		//then
+	}
+
+	@Test
+	void should_get_2_employees_when_perform_get_by_page_given_employees() throws Exception {
+		//given
+		Employee susan = employeeRepository.create(new Employee(1, "Susan", 20, "Female", 10000));
+		Employee bob = employeeRepository.create(new Employee(2, "Bob", 20, "Male", 10000));
+		Employee peter = employeeRepository.create(new Employee(3, "Peter", 22, "Male", 44));
+		Employee sam = employeeRepository.create(new Employee(2, "Sam", 23, "Male", 45));
+		Employee ted = employeeRepository.create(new Employee(3, "Ted", 20, "Male", 10000));
+		//when
+		client.perform(MockMvcRequestBuilders.get("/employees?page={page}&pageSize={pageSize}", "2", "2"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].age", containsInAnyOrder(22,23)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].gender", containsInAnyOrder("Male", "Male")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].salary", containsInAnyOrder(44, 45)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[*].name", containsInAnyOrder("Peter", "Sam")));
+
 	}
 
 }
