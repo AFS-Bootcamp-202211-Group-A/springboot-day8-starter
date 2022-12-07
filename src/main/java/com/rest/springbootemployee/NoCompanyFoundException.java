77eab6bfacf9065c772592bfc53acbd63b317d03
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-07 20:28:28 +0800

    [mockito]feat: finished global exception

--- src/main/java/com/rest/springbootemployee/NoCompanyFoundException.java
+++ src/main/java/com/rest/springbootemployee/NoCompanyFoundException.java
@@ -1,6 +1,6 @@
 package com.rest.springbootemployee;
-
 public class NoCompanyFoundException extends RuntimeException {
+
     public NoCompanyFoundException() {
         super("No company found");
     }
