85e85ecc48cdfadf4419de232513745f51734c2e
kwokth4 <kwokth4@oocl.com>
2022-12-08 01:24:26 +0800

    feat: should_get_all_companies_when_get_given_companies

--- src/test/java/com/rest/springbootemployee/CompanyServiceTest.java
+++ src/test/java/com/rest/springbootemployee/CompanyServiceTest.java
@@ -1,8 +1,40 @@
 package com.rest.springbootemployee;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class CompanyServiceTest {
+    @Mock
+    CompanyRepository companyRepository;
+
+    @InjectMocks
+    CompanyService companyService;
+
+    @Test
+    void should_get_all_companies_when_get_given_companies(){
+        //given
+        List<Company> companies = new ArrayList<>();
+        ArrayList<Employee> employeesOfCompany1 = new ArrayList<>();
+        employeesOfCompany1.add(new Employee(1, "Carlos", 26, "Male", 70000));
+        employeesOfCompany1.add(new Employee(2, "Nicole", 22, "Female", 80000));
+        companies.add(new Company(100, "spring", employeesOfCompany1));
+        companies.add(new Company(101, "autumn", employeesOfCompany1));
+        when(companyRepository.findAll()).thenReturn(companies);
+        //when
+        List<Company> resultCompanies = companyService.findAll();
+        //then
+        verify(companyRepository).findAll();
+        assertEquals(companies,resultCompanies);
+    }
 }
